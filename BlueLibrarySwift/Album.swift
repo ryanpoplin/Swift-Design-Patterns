ee855421d6217d504ea16f2d4d24b7ca892b3117
Ryan Poplin <ryanpoplin@Ryans-Mac-mini.local>
2015-12-27 03:37:17 -0500

    MVC init

---
+++ BlueLibrarySwift/Album.swift
@@ -0,0 +1,37 @@
+//
+//  Album.swift
+//  BlueLibrarySwift
+//
+//  Created by Ryan Poplin on 12/27/15.
+//  Copyright © 2015 Raywenderlich. All rights reserved.
+//
+
+import Foundation
+
+class Album {
+    
+    // MARK: - class properties
+    
+    let title: String!
+    let artist: String!
+    let genre: String!
+    let coverUrl: String!
+    let year: String!
+    
+    // MARK: - computed properties
+    
+    var description: String {
+        return "title: \(title)" + "artist: \(artist)" + "genre: \(genre)" + "coverUrl: \(coverUrl)" + "year: \(year)"
+    }
+    
+    // MARK: - class methods
+    
+    init(title: String, artist: String, genre: String, coverUrl: String, year: String) {
+        self.title = title
+        self.artist = artist
+        self.genre = genre
+        self.coverUrl = coverUrl
+        self.year = year
+    }
+    
+}
