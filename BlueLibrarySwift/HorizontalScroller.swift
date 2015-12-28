a005714f195c010bb721930fa586e7ae2b87a4cb
Ryan Poplin <ryanpoplin@Ryans-Mac-mini.local>
2015-12-28 05:23:29 -0500

    moving

---
+++ BlueLibrarySwift/HorizontalScroller.swift
@@ -0,0 +1,99 @@
+//
+//  HorizontalScroller.swift
+//  BlueLibrarySwift
+//
+//  Created by Ryan Poplin on 12/28/15.
+//  Copyright © 2015 Raywenderlich. All rights reserved.
+//
+
+import UIKit
+
+@objc protocol HorizontalScrollerDelegate {
+    func numberOfViewsForHorizontalScroller(scroller: HorizontalScroller) -> Int
+    func horizontalScrollerViewAtIndex(scroller: HorizontalScroller, index: Int) -> UIView
+    func horizontalScrollerClickedViewAtIndex(scroller: HorizontalScroller, index: Int)
+    optional func initialViewIndex(scroller: HorizontalScroller) -> Int
+}
+
+class HorizontalScroller: UIView {
+   
+    // weak!
+    weak var delegate: HorizontalScrollerDelegate?
+    
+    private let VIEW_PADDING = 10
+    private let VIEW_DIMENSIONS = 100
+    private let VIEWS_OFFSET = 100
+    
+    private var scroller: UIScrollView!
+    
+    var viewArray = [UIView]()
+ 
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        //initializedScrollView()
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)!
+        //initializedScrollView()
+    }
+    
+    func initializedScrollView() {
+    
+        scroller = UIScrollView()
+        addSubview(scroller)
+        // apply our own constraints
+        scroller.translatesAutoresizingMaskIntoConstraints = false
+        
+        addConstraint(NSLayoutConstraint(item: scroller, attribute: .Leading, relatedBy: .Equal, toItem: self, attribute: .Leading, multiplier: 1.0, constant: 0.0))
+        addConstraint(NSLayoutConstraint(item: scroller, attribute: .Trailing, relatedBy: .Equal, toItem: self, attribute: .Trailing, multiplier: 1.0, constant: 0.0))
+        addConstraint(NSLayoutConstraint(item: scroller, attribute: .Top, relatedBy: .Equal, toItem: self, attribute: .Top, multiplier: 1.0, constant: 0.0))
+        addConstraint(NSLayoutConstraint(item: scroller, attribute: .Bottom, relatedBy: .Equal, toItem: self, attribute: .Bottom, multiplier: 1.0, constant: 0.0))
+        
+        let tapRecognizer = UITapGestureRecognizer(target: self, action:Selector("scrollerTapped:"))
+        scroller.addGestureRecognizer(tapRecognizer)
+        
+    }
+    
+    func scrollerTapped(gesture: UITapGestureRecognizer) {
+        let location = gesture.locationInView(gesture.view)
+        if let delegate = delegate {
+            for index in 0..<delegate.numberOfViewsForHorizontalScroller(self) {
+                let view = scroller.subviews[index]
+                if CGRectContainsPoint(view.frame, location) {
+                    delegate.horizontalScrollerClickedViewAtIndex(self, index: index)
+                    scroller.setContentOffset(CGPoint(x: view.frame.origin.x - self.frame.size.width / 2 + view.frame.size.width / 2, y: 0), animated:true)
+                    break
+                }
+            }
+        }
+    }
+    
+    func viewAtIndex(index :Int) -> UIView {
+        return viewArray[index]
+    }
+    
+    func reload() {
+        if let delegate = delegate {
+            viewArray = []
+            let views = scroller.subviews
+            for view in views {
+                view.removeFromSuperview()
+            }
+            var xValue = VIEWS_OFFSET
+            for index in 0..<delegate.numberOfViewsForHorizontalScroller(self) {
+                xValue += VIEW_PADDING
+                let view = delegate.horizontalScrollerViewAtIndex(self, index: index)
+                view.frame = CGRectMake(CGFloat(xValue), CGFloat(VIEW_PADDING), CGFloat(VIEW_DIMENSIONS), CGFloat(VIEW_DIMENSIONS))
+                scroller.addSubview(view)
+                xValue += VIEW_DIMENSIONS + VIEW_PADDING
+                viewArray.append(view)
+            }
+            scroller.contentSize = CGSizeMake(CGFloat(xValue + VIEWS_OFFSET), frame.size.height)
+            if let initialView = delegate.initialViewIndex?(self) {
+                scroller.setContentOffset(CGPoint(x: CGFloat(initialView)*CGFloat((VIEW_DIMENSIONS + (2 * VIEW_PADDING))), y: 0), animated: true)
+            }
+        }
+    }
+    
+}
